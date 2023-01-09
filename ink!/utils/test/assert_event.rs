7e5238953547779f1c52be10a3eab206a0050e62
default <dalvath3700@gmail.com>
2023-01-09 13:27:26 +0530

    updated with attribute name

--- ink!/utils/test/assert_event.rs
+++ ink!/utils/test/assert_event.rs
@@ -3,90 +3,97 @@
 */
 
 // E.g. `Transfer` event
+#[ink(event)]
 pub struct Transfer {
-    [#ink()]
-    from: Some(AccountId),
-    to: Some(AccountId),
-    from: Some(AccountId),
+    [#ink(topic)]
+    from: Option<AccountId>,
+    [#ink(topic)]
+    to: Option<AccountId>,
+    value: Balance,
 }
 
-// Create a function for `Transfer` event
-fn assert_transfer_event(
-    event: &ink::env::test::EmittedEvent,
-    expected_from: Option<AccountId>,
-    expected_to: Option<AccountId>,
-    expected_value: Balance,
-) {
-    let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
-        .expect("encountered invalid contract event data buffer");
-    if let Event::Transfer(Transfer { from, to, value }) = decoded_event {
-        assert_eq!(from, expected_from, "encountered invalid Transfer.from");
-        assert_eq!(to, expected_to, "encountered invalid Transfer.to");
-        assert_eq!(value, expected_value, "encountered invalid Trasfer.value");
-    } else {
-        panic!("encountered unexpected event kind: expected a Transfer event")
-    }
+#[cfg(test)]
+mod tests {
 
-    fn encoded_into_hash<T>(entity: &T) -> Hash
-    where
-        T: scale::Encode,
-    {
-        let mut result = Hash::clear();
-        let len_result = result.as_ref().len();
-        let encoded = entity.encode();
-        let len_encoded = encoded.len();
-        if len_encoded <= len_result {
-            result.as_mut()[..len_encoded].copy_from_slice(&encoded);
-            return result;
+    // Create a function for `Transfer` event
+    fn assert_transfer_event(
+        event: &ink::env::test::EmittedEvent,
+        expected_from: Option<AccountId>,
+        expected_to: Option<AccountId>,
+        expected_value: Balance,
+    ) {
+        let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
+            .expect("encountered invalid contract event data buffer");
+        if let Event::Transfer(Transfer { from, to, value }) = decoded_event {
+            assert_eq!(from, expected_from, "encountered invalid Transfer.from");
+            assert_eq!(to, expected_to, "encountered invalid Transfer.to");
+            assert_eq!(value, expected_value, "encountered invalid Trasfer.value");
+        } else {
+            panic!("encountered unexpected event kind: expected a Transfer event")
         }
-        let mut hash_output = <<Blake2x256 as HashOutput>::Type as Default>::default();
-        <Blake2x256 as CryptoHash>::hash(&encoded, &mut hash_output);
-        let copy_len = core::cmp::min(hash_output.len(), len_result);
-        result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
-        result
+
+        fn encoded_into_hash<T>(entity: &T) -> Hash
+        where
+            T: scale::Encode,
+        {
+            let mut result = Hash::clear();
+            let len_result = result.as_ref().len();
+            let encoded = entity.encode();
+            let len_encoded = encoded.len();
+            if len_encoded <= len_result {
+                result.as_mut()[..len_encoded].copy_from_slice(&encoded);
+                return result;
+            }
+            let mut hash_output = <<Blake2x256 as HashOutput>::Type as Default>::default();
+            <Blake2x256 as CryptoHash>::hash(&encoded, &mut hash_output);
+            let copy_len = core::cmp::min(hash_output.len(), len_result);
+            result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
+            result
+        }
+
+        let expected_topics = [
+            encoded_into_hash(&PrefixedValue {
+                prefix: b"",
+                value: b"Erc20::Transfer",
+            }),
+            encoded_into_hash(&PrefixedValue {
+                prefix: b"Erc20::Transfer::from",
+                value: &expected_from,
+            }),
+            encoded_into_hash(&PrefixedValue {
+                prefix: b"Erc20::Transfer::to",
+                value: &expected_to,
+            }),
+            encoded_into_hash(&PrefixedValue {
+                prefix: b"Erc20::Transfer::value",
+                value: &expected_value,
+            }),
+        ];
+        for (n, (actual_topic, expected_topic)) in event.topics.iter().zip(expected_topics).enumerate()
+        {
+            let topic = <Hash as scale::Decode>::decode(&mut &actual_topic[..])
+                .expect("encountered invalid topic encoding");
+            assert_eq!(topic, expected_topic, "encountered invalid topic at {}", n);
+        }
     }
 
-    let expected_topics = [
-        encoded_into_hash(&PrefixedValue {
-            prefix: b"",
-            value: b"Erc20::Transfer",
-        }),
-        encoded_into_hash(&PrefixedValue {
-            prefix: b"Erc20::Transfer::from",
-            value: &expected_from,
-        }),
-        encoded_into_hash(&PrefixedValue {
-            prefix: b"Erc20::Transfer::to",
-            value: &expected_to,
-        }),
-        encoded_into_hash(&PrefixedValue {
-            prefix: b"Erc20::Transfer::value",
-            value: &expected_value,
-        }),
-    ];
-    for (n, (actual_topic, expected_topic)) in event.topics.iter().zip(expected_topics).enumerate()
-    {
-        let topic = <Hash as scale::Decode>::decode(&mut &actual_topic[..])
-            .expect("encountered invalid topic encoding");
-        assert_eq!(topic, expected_topic, "encountered invalid topic at {}", n);
+
+    // Use like this inside a `#[test]` attributed function
+    #[test]
+    fn transfer_works() {
+        // ----snip---
+
+
+        // Transfer event triggered during initial construction.
+        let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+        assert_eq!(1, emitted_events.len());
+        
+        assert_transfer_event(
+            &emitted_events[0],
+            None,
+            Some(AccountId::from([0x01; 32])),
+            100,
+        );
     }
 }
 
-
-// Use like this inside a `#[test]` function inside `#[cfg(tests)]` attributed module
-#[test]
-fn transfer_works() {
-    // ----snip---
-
-
-    // Transfer event triggered during initial construction.
-    let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
-    assert_eq!(1, emitted_events.len());
-    
-    assert_transfer_event(
-        &emitted_events[0],
-        None,
-        Some(AccountId::from([0x01; 32])),
-        100,
-    );
-}
