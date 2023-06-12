b755bde5d45e158813da15d87e4b43b3b7f186ab
abhi3700 <dalvath3700@gmail.com>
2023-06-12 23:40:40 +0530

    how to call extrinsic from inherent functions

---
+++ frame-pallet/scaffoldings/runtime/extrinsic_call_frm_inherent.rs
@@ -0,0 +1,4 @@
+//! Extrinsic call from inherent function
+//! Code line: https://github.com/paritytech/substrate/blob/85415fb3a452dba12ff564e6b093048eed4c5aad/frame/timestamp/src/lib.rs#L233-L234
+
+Some(Call::set { now: next_time })
